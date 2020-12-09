f7cd7640d0fe82169112ec68901e72728a45cace
Jack Hubert <jphubert93@gmail.com>
2020-12-09 12:50:55 -0700

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,38 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  first_name, last_name = data[season].find{|contestant| contestant["status"] == "Winner"}["name"].split(' ')
+  first_name
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestant_pool|
+    if contestant_pool.any?{|contestant| contestant["occupation"] == occupation}
+      return contestant_pool.find{|contestant| contestant["occupation"] == occupation}["name"]
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  number_contestants = 0
+
+  data.each do |season, contestant_pool|
+    number_contestants += contestant_pool.filter{|contestant| contestant["hometown"] == hometown}.length
+  end
+
+  number_contestants
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestant_pool|
+    if contestant_pool.any?{|contestant| contestant["hometown"] == hometown}
+      return contestant_pool.find{|contestant| contestant["hometown"] == hometown}["occupation"]
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age_pool = []
+  data[season].each{|contestant| age_pool << contestant["age"].to_i}
+  (age_pool.reduce(:+).to_f/age_pool.length).round()
 end
